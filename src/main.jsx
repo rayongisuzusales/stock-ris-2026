6a419d89422c2543024bce1c6eb3da00c4648489
rayongisuzusales <risdgmteam@gmail.com>
2026-05-29 07:35:51 +0700

    feat: src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { StockProvider } from './context/StockContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <StockProvider>
+        <App />
+      </StockProvider>
+    </AuthProvider>
   </React.StrictMode>
 )
